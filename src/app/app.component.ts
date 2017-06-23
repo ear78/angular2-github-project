fd7aa3144054fe8061bfa40cc494389658b5fa29
elliot richardson <ear78@aol.com>
2017-06-23 00:17:32 -0500

    created service for http request, added navbar to profile component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { GithubService } from './services/github.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<profile></profile>',
+  styleUrls: ['./app.component.css'],
+  providers: [GithubService]
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent {}
---
+++ src/app/services/github.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService {
+    private username:string;
+    private client_id = '6c68b39c7e427b7538a1';
+    private client_secret = '71c4453fd12bdf3ebc2fda73f8fb67130024dbbf';
+
+    constructor(private _http: Http){
+        console.log('Github Service Ready...');
+        this.username = 'ear78';
+    }
+
+    getUser(){
+        return this._http.get('http://api.github.com/users/' + this.username + '?client_id=' + this.client_id + '&client_secret=' + this.client_secret)
+        .map(res => res.json());
+    }
+}
